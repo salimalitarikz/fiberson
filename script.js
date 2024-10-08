c44415d29f0a1211efa18b1af7f7464178c752fe
salimalitarikz <salimalitarikz@gmail.com>
2024-10-08 21:24:42 +0300

    081024-f3

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var menu_button = document.getElementById("menu_button");
 var menu = document.getElementById("menu");
 menu_button.addEventListener("click", function(){
     menu.classList.toggle("opened_menu");
+    menu_button.classList.toggle("opened_menu_button");
 });
 
 
